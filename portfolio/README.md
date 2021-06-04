68939428d8850d42778dd4940d17ea9640d3dd16
Iann <sebilango2000@gmail.com>
2021-06-04 10:41:33 +0000

    photos and information update

---
+++ portfolio/README.md
@@ -0,0 +1,8 @@
+This repo contains Iann's portfolio ans SPS projects.
+
+By default it contains a barebones portfolio web app. To run a local server,
+execute this command:
+
+```
+mvn package exec:java
+```
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['“I am Optimus Prime, and I send this message so that our pasts will always be remembered, for in those memories we live on."'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
