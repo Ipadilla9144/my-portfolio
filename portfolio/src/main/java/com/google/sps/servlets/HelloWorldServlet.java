429a95156d3e0f524ab409ecf658805fc440bb8f
Iann <sebilango2000@gmail.com>
2021-06-11 06:53:52 +0000

    Servlet modification

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -13,6 +13,9 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Hello Iann Sebastian!</h1>");
+    response.getWriter().println("<h1>You must be the change you wish to see in the world.</h1>");
+    response.getWriter().println("<h1>That which does not kill us makes us stronger.</h1>");
+    response.getWriter().println("<h1>There are mysteries to the universe we were never meant to solve, but who we are and why we are here are not among them. Those answers we carry inside.</h1>");
   }
 }
